eb11d1c7c0e1ad71b23f77a5b082c850d5afcb1a
Joshua Chong Toua <chongtouajoshua@gmail.com>
2023-05-22 12:48:12 +0200

    Formulaire ok

---
+++ inc/manager-db.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'connect-db.php';
+function getVerification($login,$password)
+{
+    global $pdo;
+    $query = "SELECT * FROM users WHERE login=:login and password=:password";
+    $prep = $pdo->prepare($query);
+    $prep->bindValue(':login', $login, PDO::PARAM_STR);
+    $prep->bindValue(':password', $password, PDO::PARAM_STR);
+    $prep->execute();
+    // on vérifie que la requête ne retourne qu'une seule ligne
+    if ($prep->rowCount() == 1) {
+        $result = $prep->fetch();
+        return $result;
+    } else {
+        return false;
+    }
+}
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+require_once "inc/manager-db.php";
+?>
+
+<form method="POST">
+    Login : <input type="text" name="login" >
+    Password : <input type="password" name="password" >
+    <input type="submit" name="submit" value="submit">
+
+</form>
+
+<?php
+if (!empty($_POST['login'])){
+    $login=$_POST['login'];
+}
+else
+    $login='';
+
+if (!empty($_POST['password'])){
+    $password=$_POST['password'];
+}
+else
+    $password='';
+
+echo"$login $password";
+?>
+<br>
+<?php
+
+if (getVerification($login,$password)){
+    echo"ok";
+}
+
+else
+    echo"ko";
