77357f41d7b52fb0cc73e8793ef7f3f7371e5db3
Joshua Chong Toua <chongtouajoshua@gmail.com>
2023-05-23 11:44:57 +0200

    update delete ok

--- userCRUD.php
+++ userCRUD.php
@@ -2,21 +2,22 @@
 require_once "includes/Users.php";
 var_dump($_POST);
 
-echo "<pre>".print_r( $_GET, true)."</pre>";
+echo "<pre>" . print_r($_GET, true) . "</pre>";
+echo "<pre>" . print_r($_POST, true) . "</pre>";
 
 
 $displayForm = false;
 
-if(isset($_GET['action'])){
+if (isset($_GET['action'])) {
     $action = $_GET['action'];
 }
 
-if(isset($_GET['user_id'])){
+if (isset($_GET['user_id'])) {
     $id = $_GET['user_id'];
 }
 
 
-if(isset($action)) {
+if (isset($action)) {
     if ($action == "create") {
         $displayForm = true;
         // si le formulaire a été submit
@@ -38,19 +39,16 @@ if(isset($action)) {
             $displayForm = false;
 
         }
-    } else if ($action == "update" && !empty($id)) {
-        $displayForm = true;
-
-        $infoUser = getUser($id);
-
+    } else if ($action == "update") {
         // si le formulaire a été submit
         if (isset($_POST['login']) && isset($_POST['password'])) {
             $login = $_POST['login'];
             $password = $_POST['password'];
+            $id = $_POST['user_id'];
 
 
 
-            $sql = "UPDATE users SET login=:login, password=:password, type_id=:type_id WHERE id=:id;";
+            $sql = "UPDATE users SET login=:login, password=:password WHERE user_id=:id;";
             try {
                 global $pdo;
                 $statement = $pdo->prepare($sql);
@@ -63,47 +61,51 @@ if(isset($action)) {
                 echo 'Erreur : ' . $e->getMessage();
             }
 
-        } else if ($action == "delete" && !empty($id)) {
+        } else {
+            $displayForm = true;
+            $infoUser = getUser($id);
+        }
+    } else if ($action == "delete" && !empty($id)) {
 
-            $sql = "DELETE FROM users WHERE user_id = :id";
+        $sql = "DELETE FROM users WHERE user_id = :id";
 
-            try {
-                global $pdo;
-                $statement = $pdo->prepare($sql);
-                $statement->bindParam(':id', $id, PDO::PARAM_INT);
-                $statement->execute();
-            } catch (PDOException $e) {
-                die("erreur dans la requete " . $e->getMessage());
-            }
-
+        try {
+            global $pdo;
+            $statement = $pdo->prepare($sql);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+            $statement->execute();
+        } catch (PDOException $e) {
+            die("erreur dans la requete " . $e->getMessage());
         }
 
-
     }
+
+
 }
 
 
-    $users = getAllUsers();
+$users = getAllUsers();
 
 
-    if ($displayForm) {
-        echo '
+if ($displayForm) {
+    echo '
     <form method="POST" action="?action=' . $action . '">
         Login : <input type="text" name="login"  value="' . ($action == 'update' ? $infoUser->login : '') . '" />
         <br>
         Password : <input type="password" name="password"  value="' . ($action == 'update' ? $infoUser->password : '') . '">
         <br>
+        <input type="hidden" name="user_id" value="' . ($action == 'update' ? $id : '' ) . '">
         <!--Role:
         <select name="role">
         <option value ="admin" >admin</option>
         <option value="visiteur">visiteur</option>
         </select>
-        
+        -->
         <input type="submit" name="submit" value="submit">
-    </form> -->
+    </form>
     ';
-    } else {
-        echo '
+} else {
+    echo '
     <table>
         <tr>
             <th>user_id</th>
@@ -116,22 +118,22 @@ if(isset($action)) {
         </tr>
     ';
 
-        foreach ($users as $user) {
-            echo '<tr>';
-            echo '<td>' . $user->user_id . '</td>';
-            echo '<td>' . $user->login . '</td>';
-            echo '<td>' . $user->password . '</td>';
-            echo '<td>' . $user->type_id . '</td>';
-            echo '<td>' . $user->create_date . '</td>';
-            echo '<td> <a href="?action=update&user_id=' . $user->user_id . '">edit</a> </td>';
-            echo '<td> <a href="?action=delete&user_id=' . $user->user_id . '">delete</a> </td>';
-            echo '</tr>';
-        }
-
-        echo '</table>';
+    foreach ($users as $user) {
+        echo '<tr>';
+        echo '<td>' . $user->user_id . '</td>';
+        echo '<td>' . $user->login . '</td>';
+        echo '<td>' . $user->password . '</td>';
+        echo '<td>' . $user->type_id . '</td>';
+        echo '<td>' . $user->create_date . '</td>';
+        echo '<td> <a href="?action=update&user_id=' . $user->user_id . '">edit</a> </td>';
+        echo '<td> <a href="?action=delete&user_id=' . $user->user_id . '">delete</a> </td>';
+        echo '</tr>';
     }
 
-    echo '<a href="?action=create">Create</a>' ;
+    echo '</table>';
+}
+
+echo '<a href="?action=create">Create</a>';
 ?>
 
 
